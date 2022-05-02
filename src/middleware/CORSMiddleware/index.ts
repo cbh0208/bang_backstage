d819759add33573177dd89fb058b57bd5092b8f2
gongshu <1078419631@qq.com>
2022-05-02 23:30:01 +0800

    fix cors

--- src/middleware/CORSMiddleware/index.ts
+++ src/middleware/CORSMiddleware/index.ts
@@ -3,6 +3,7 @@ import { BaseContext } from "koa"
 /** cors 中间件 */
 export default async function (ctx: BaseContext, next: () => Promise<any>) {
     ctx.set('Access-Control-Allow-Origin','*')
+    ctx.set('Access-Control-Allow-Headers','authorization,Content-Type')
     await next()
 
     
